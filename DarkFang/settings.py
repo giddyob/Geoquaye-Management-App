4793fd1f72d8d9339fd1be6b0ab44839e488fdbb
giddob <gideonoblokuteye@gmail.com>
2022-09-13 21:32:33 +0000

    made some changes

--- DarkFang/settings.py
+++ DarkFang/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = ['127.0.0.1', 'geoquayewma.herokuapp.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -44,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
